0aa3fe39bcacf141ddd01bc6b60a895aaba2fc3b
avinashpitti <avinash87129@gmail.com>
2026-02-26 22:56:45 +0530

    practice selectors

---
+++ class 1/script.js
@@ -0,0 +1,13 @@
+// document.getElementById('para').style.display='none';
+
+document.getElementById('para').innerHTML='I have changed the content';
+
+document.getElementsByClassName('info')[0].innerHTML='Info of h2 is changed';
+document.getElementsByClassName('info')[1].innerHTML='Info of h3 is changed';
+
+
+document.querySelectorAll('.matter')[0].innerHTML='used queryselectorall';
+document.querySelectorAll('.matter')[1].innerHTML='used queryselectorall';
+document.querySelectorAll('.matter')[2].innerHTML='used queryselectorall';
+
+
